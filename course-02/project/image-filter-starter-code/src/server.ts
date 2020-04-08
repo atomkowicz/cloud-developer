760c1e90e8537497154f5441ff626ed4edb3c49d
Agnieszka Tomkowicz <agntomk@gmail.com>
2020-04-08 18:41:02 +0200

    Feat: add image filter endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,38 +9,44 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //
+  app.get("/filteredimage/", async (req, res) => {
+    const { image_url } = req.query;
 
-  /**************************************************************************** */
+    //    validate the image_url query
+    const isUrlValid = image_url && /^http[s]:\/\//.test(image_url);
+    if (!isUrlValid) res.send("Url is not a valid url");
 
-  //! END @TODO1
-  
+    //    call filterImageFromURL(image_url) to filter the image
+    const processedImage = await filterImageFromURL(image_url);
+
+    //    send the resulting file in the response
+    //    deletes any files on the server on finish of the response
+    res.sendFile(processedImage, async () =>{
+      try {
+        const isImageDeleted = await deleteLocalFiles([processedImage]);
+      } catch(e) {
+        console.log(e)
+      }
+    });
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
