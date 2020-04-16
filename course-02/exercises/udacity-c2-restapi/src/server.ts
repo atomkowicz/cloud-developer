0db52e8894fb01a2fe65bcbd76cbfa1cf37c9ad6
Agnieszka Tomkowicz <agntomk@gmail.com>
2020-04-16 20:41:38 +0200

    Feat: update cors

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
+
 import { sequelize } from './sequelize';
-
 import { IndexRouter } from './controllers/v0/index.router';
-
 import bodyParser from 'body-parser';
-
 import { V0MODELS } from './controllers/v0/model.index';
 
 (async () => {
@@ -16,9 +14,9 @@ import { V0MODELS } from './controllers/v0/model.index';
   
   app.use(bodyParser.json());
 
-  //CORS Should be restricted
+  //CORS Should be restricted, for excrcise we allow all
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
@@ -30,7 +28,6 @@ import { V0MODELS } from './controllers/v0/model.index';
     res.send( "/api/v0/" );
   } );
   
-
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
